43881b08611ff13be7fb038d060b0ef3aeea2747
Marcos G <warcosyt@gmail.com>
2021-11-02 07:52:36 +0100

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const basicCards = require('./data/basicCards'); //phantom cards available for the players but not visible in the api endpoint
 
-getPlayerCards = (username, oneDayAgo) => (fetch(`https://game-api.splinterlands.io/cards/collection/${username}`,
+getPlayerCards = (username, oneDayAgo) => (fetch(`https://api2.splinterlands.com/cards/collection/${username}`,
   { "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
   .then(x => x && x.json())
   .then(x => x['cards'] ? x['cards'].filter(x=>(x.delegated_to === null || x.delegated_to === username)
@@ -9,8 +9,8 @@ getPlayerCards = (username, oneDayAgo) => (fetch(`https://game-api.splinterlands
   && (!(x.last_used_player !== username && Date.parse(x.last_used_date) > oneDayAgo))).map(card => card.card_detail_id) : '')
   .then(advanced => basicCards.concat(advanced))
    .catch(e=> {
-    console.log('Error: game-api.splinterlands did not respond trying api.slinterlands... ');
-    fetch(`https://api.splinterlands.io/cards/collection/${username}`,
+    console.log('Error: api2.splinterlands did not respond trying game-api.slinterlands... ');
+    fetch(`https://game-api.splinterlands.io/cards/collection/${username}`,
       { "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
       .then(x => x && x.json())
       .then(x => x['cards'] ? x['cards'].filter(x=>(x.delegated_to === null || x.delegated_to === username) 
