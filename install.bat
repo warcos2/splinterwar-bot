00cc6ffb2d25ebaafb4e664d9bc89023c92845f7
Marcos G <warcosyt@gmail.com>
2021-11-04 13:47:19 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo off
 echo --------------------------------------------------------------------------
 echo SplinterWar-Bot by Warcos (Based on bot by alfficcadenti and PCJones)
+echo Check github page for updates: https://github.com/warcos2/splinterwar-bot
 echo Join our discord server https://discord.gg/8Kf3wuta5Z
-echo                    Close this window to stop the bot
 echo --------------------------------------------------------------------------
 npm install
